5f4eab152c025af41c94dd63cf8911ef33fda683
Deepak Venugopal <deepakvenugopal90@gmail.com>
2020-06-12 22:11:30 +0530

    Add files via upload

---
+++ OrderProcessor.UnitTests/ProcessorTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OrderProcessor.UnitTests
+{
+    [TestClass]
+    public class ProcessorTest
+    {
+        [TestMethod]
+        public void InitiateCheckout_BookOrder_ReturnsTrue()
+        {
+            // Arrange
+            Processor process = new Processor();
+            List<Product> products = new List<Product> {
+                new Product { Name="Twilight", Type=Enum.ProductType.BookOrder}
+                };
+
+            // Act
+            var expected = process.InitiateCheckout(products);
+
+            // Assert
+            Assert.IsTrue(expected);
+        }
+    }
+} 
---
+++ OrderProcessor.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OrderProcessor.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OrderProcessor.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0224e2f6-59fc-4d20-afa2-04ffe5f8b993")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
